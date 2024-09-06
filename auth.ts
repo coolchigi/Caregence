0423f120368c8a1d713a45761c8acbfeee66fc14
Chigo <72313512+coolchigi@users.noreply.github.com>
2024-09-06 19:46:11 -0400

    #1 Add auth using next auth

---
+++ auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {params: {prompt: "consent"}},
+    }),
+  ],
+})
